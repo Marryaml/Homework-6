f33922a052eba93f548dc7eb62a03f39488b89d4
Marryaml <94197586+Marryaml@users.noreply.github.com>
2022-03-10 22:56:16 +0100

    2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function getPosts() {
 
         let response = this.responseText;
         let responseData = JSON.parse(response);
+
         console.log(responseData);
 
         let ul = document.createElement('ul');
@@ -15,10 +16,47 @@ function getPosts() {
 
         document.getElementById('api').appendChild(ul);
 
-  });
+    });
 
+    requist.addEventListener('error', function() {
+        let p = document.createElement ('p');
+        p.textContent = 'Error';
 
+        document.getElementById('api').appendChild(p);
+    })
 
+    requist.open('GET' , 'https://reqres.in/api/users?page=2');
 
-  
+    requist.send();
 }
+
+// Fetch
+
+let lastPage = 1;
+
+
+function getUsers(page){
+    fetch('https://reqres.in/api/users?page=' + page, {
+        method: 'go'
+    })
+    .then(function(response){
+        if(response.status !== 300){
+            throw 'Failed';
+        }
+        return response.json();
+    })
+    .then(function(responseData){
+        var fragment = document.createDocumentFragment();
+
+
+        responseData.data.array.forEach(x => {
+            let li = document.createElement('li');
+            li.textContent = x.email;
+
+            fragment.appendChild(li);
+        });
+   
+
+    })
+}
+
